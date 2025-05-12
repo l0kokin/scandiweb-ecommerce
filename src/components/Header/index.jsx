e4f073d6d93728cd8abbb821c5e6b671827ef243
l0kokin <sali.gogishvili1@gmail.com>
2025-05-12 13:39:40 +0400

    style: finish styling header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,19 @@
 import { NavLink } from "react-router-dom";
-import Cart from "../icons/Cart";
 import Logo from "../../assets/images/logo.png";
+import CartButton from "../CartButton";
 
 const Header = () => {
   return (
-    <header className="bg-white py-6">
+    <header className="bg-white pt-6">
       <div className="container mx-auto px-4 flex justify-between items-center">
         <nav className="flex">
           <NavLink
             to="/women"
             className={({ isActive }) =>
-              `uppercase pt-1 pb-8 px-4 ${
-                isActive ? "border-b-2 border-green text-green" : "text-primary"
+              `uppercase pt-1 pb-8 px-4 leading-[1.2] ${
+                isActive
+                  ? "border-b-2 border-green text-green font-semibold"
+                  : "text-primary font-normal"
               }`
             }
             data-testid="category-link"
@@ -22,8 +24,10 @@ const Header = () => {
           <NavLink
             to="/men"
             className={({ isActive }) =>
-              `uppercase pt-1 pb-8 px-4 ${
-                isActive ? "border-b-2 border-green text-green" : "text-primary"
+              `uppercase pt-1 pb-8 px-4 leading-[1.2] ${
+                isActive
+                  ? "border-b-2 border-green text-green font-semibold"
+                  : "text-primary font-normal"
               }`
             }
             data-testid="category-link"
@@ -34,8 +38,10 @@ const Header = () => {
           <NavLink
             to="/kids"
             className={({ isActive }) =>
-              `uppercase pt-1 pb-8 px-4 ${
-                isActive ? "border-b-2 border-green text-green" : "text-primary"
+              `uppercase pt-1 pb-8 px-4 leading-[1.2] ${
+                isActive
+                  ? "border-b-2 border-green text-green font-semibold"
+                  : "text-primary font-normal"
               }`
             }
             data-testid="category-link"
@@ -47,9 +53,7 @@ const Header = () => {
         <div>
           <img src={Logo} alt="logo" />
         </div>
-        <button>
-          <Cart />
-        </button>
+        <CartButton />
       </div>
     </header>
   );
