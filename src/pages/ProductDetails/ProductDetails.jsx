6322182c64340e6af61a6e96034d4ee1f95482ae
l0kokin <sali.gogishvili1@gmail.com>
2025-05-13 15:20:31 +0400

    refactor: move product gallery to a seperate component

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -3,14 +3,13 @@ import { useParams, useNavigate } from "react-router-dom";
 import dummyData from "../Home/dummyData";
 import { Header } from "../../components/Header";
 import { Loader } from "../../Loader";
-import { LeftArrow, RightArrow } from "../../components/icons";
+import { ProductGallery } from "../../components/Product";
 
 const ProductDetails = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [product, setProduct] = useState(null);
   const [selectedAttributes, setSelectedAttributes] = useState({});
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -70,76 +69,7 @@ const ProductDetails = () => {
       <div className="mx-25 py-8 px-4">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-28 lg:gap-10">
           {/* Gallery */}
-          <section className="lg:col-span-2 flex flex-col md:flex-row">
-            <div className="flex md:flex-col order-2 md:order-1 space-x-2 md:space-x-10 md:space-y-5 mr-0 md:mr-4">
-              {product.gallery.map((image, index) => (
-                <button
-                  key={index}
-                  onClick={() => setCurrentImageIndex(index)}
-                  className={`w-16 h-16 border ${
-                    currentImageIndex === index
-                      ? "border-primary"
-                      : "border-transparent"
-                  }`}
-                  data-testid={`gallery-thumbnail-${index}`}
-                >
-                  <img
-                    src={image}
-                    alt={`Thumbnail ${index + 1}`}
-                    className="w-full h-full object-cover"
-                  />
-                </button>
-              ))}
-            </div>
-
-            <div className="relative md:order-2">
-              <img
-                src={product.gallery[currentImageIndex]}
-                alt={product.name}
-                className="w-full h-full object-cover"
-                data-testid="product-gallery"
-              />
-              <button
-                onClick={() =>
-                  setCurrentImageIndex(
-                    (prevIndex) =>
-                      (prevIndex - 1 + product.gallery.length) %
-                      product.gallery.length
-                  )
-                }
-                className="absolute px-3 py-2 bg-gray-dark top-1/2 left-2 transform -translate-y-1/2 z-10"
-                aria-label="Previous image"
-              >
-                <LeftArrow />
-              </button>
-
-              <button
-                onClick={() =>
-                  setCurrentImageIndex(
-                    (prevIndex) => (prevIndex + 1) % product.gallery.length
-                  )
-                }
-                className="absolute px-3 py-2 bg-gray-dark top-1/2 right-2 transform -translate-y-1/2 z-10"
-                aria-label="Next image"
-              >
-                <RightArrow />
-              </button>
-
-              {!product.inStock && (
-                <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-70">
-                  <span className="text-3xl text-gray-text absolute">
-                    OUT OF STOCK
-                  </span>
-                  <img
-                    src={product.gallery[currentImageIndex]}
-                    alt={product.name}
-                    className="w-full h-full object-cover opacity-30"
-                    data-testid="product-gallery"
-                  />
-                </div>
-              )}
-            </div>
-          </section>
+          <ProductGallery product={product} />
 
           {/* Details */}
           <section className="lg:col-span-1 w-74">
