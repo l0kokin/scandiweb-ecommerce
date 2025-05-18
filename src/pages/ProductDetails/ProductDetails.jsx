7427601fca39cb64df1c552c6bc40db5edd9fdaf
l0kokin <sali.gogishvili1@gmail.com>
2025-05-18 14:42:19 +0400

    fix: imports for loader

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import { Header } from "../../components/Header";
-import { Loader } from "../../Loader";
+import Loader from "../../components/Loader/Loader";
 import { ProductGallery } from "../../components/Product";
 import useProductDetails from "./useProductDetails";
 
