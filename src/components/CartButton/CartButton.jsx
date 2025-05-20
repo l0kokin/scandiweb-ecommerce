66ec64e6f2558c3b72bd3b8abda7dd09f0f78e1b
l0kokin <sali.gogishvili1@gmail.com>
2025-05-20 11:08:46 +0400

    style: fix styling for cart items count button

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -12,7 +12,7 @@ const CartButton = ({ itemCount }) => {
     >
       <Cart />
       {itemCount > 0 && (
-        <span className="absolute -top-3 -right-3 bg-black text-white rounded-full w-5 h-5 flex items-center justify-center">
+        <span className="absolute -top-3 -right-3 bg-black text-white rounded-full w-5 h-5 flex items-center justify-center font-bold font-roboto text-sm">
           {itemCount}
         </span>
       )}
